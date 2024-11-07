b7643a52acf8c54cb604f382e79e28369bab2436
ardra anish <ardraanishstackup@gmail.com>
2024-11-07 19:35:34 +0530

    issue

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
     <Provider store={Store}>
-      <CookiesProvider>
+      
         <App/>
-      </CookiesProvider>
+      
     </Provider>
 )
 
